ca3a09dcd9e879112d0c2020ab103c814dfb6f85
GingerPianist <monikazarudzka@gail.com>
2020-09-13 16:38:50 +0200

    Updates

---
+++ precache-manifest.624bc157b3042aea0d8bb2937d1a9f19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a42d1a8a012cb8286471735201c5ad91",
+    "url": "/snake/index.html"
+  },
+  {
+    "revision": "8d43ce6b5f587246825c",
+    "url": "/snake/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cb3dc652591b4fdee100",
+    "url": "/snake/static/js/2.61699a64.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/snake/static/js/2.61699a64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d43ce6b5f587246825c",
+    "url": "/snake/static/js/main.882ee157.chunk.js"
+  },
+  {
+    "revision": "330302f1424891b30bb2",
+    "url": "/snake/static/js/runtime-main.f5fb9ecf.js"
+  }
+]);
